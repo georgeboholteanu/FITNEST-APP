d88b7d8835810c0c6ff77a3ae844df94519883a4
georgeboholteanu <george.boholteanu@gmail.com>
2023-10-10 23:52:35 +0100

    configured tailwind

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../index.css";
+
+const Header = () => {
+	return (
+		<div>
+			<h1 className="text-sm font-bold underline text-red-700">
+				Hello world!
+			</h1>
+		</div>
+	);
+};
+
+export default Header;
