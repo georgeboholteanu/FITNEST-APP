f1a96fff5ff1d6cf6a4b746f6ec52b4eacc409be
georgeboholteanu <george.boholteanu@gmail.com>
2023-10-27 18:00:28 +0100

    contact component styling

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
 	return (
 		<div className="relative">
 			{/* image */}
-			<div className="object-cover">
+			<div className="">
 				<img
 					className=""
 					src="/assets/images/geert-pieters-AfgJpWQH4lw-unsplash.jpg"
@@ -14,18 +14,18 @@ const Contact = () => {
 			<div className="md:absolute md:top-1/3 mx-auto px-5 sm:px-10 md:px-20 transform md:-translate-y-1/3 text-left text-white bg-gray-400 md:bg-transparent">
 				<div className="grid grid-col-1 md:grid-cols-2 grid-flow-row md:gap-x-10 lg:gap-x-20 gap-y-5 py-10">
 					{/* hero */}
-					<div className="flex items-center w-full">
-						<div className="glass px-5 md:px-10 py-5 md:py-10 rounded-2xl shadow-lg bg-slate-500/40 ">
-							<h1 className="text-3xl md:text-5xl font-bold capitalize text-white">
-								Get in Touch for a Fit Tomorrow!
-							</h1>
+					<div className="flex items-center">
+						<div className="glass px-5 md:px-10 py-5 md:py-10 rounded-2xl shadow-lg bg-slate-500/40">
+								<h1 className="text-3xl md:text-5xl font-bold capitalize text-white">
+									Get in Touch for a Fit Tomorrow!
+								</h1>
 
-							<p className="mt-6 font-semibold text-black ">
-								Ready to kickstart your fitness adventure?
-							</p>
-							<p className=" font-semibold text-yellow-400 ">
-								We can&apos;t wait to hear from you!
-							</p>
+								<p className="mt-6 font-semibold text-black ">
+									Ready to kickstart your fitness adventure?
+								</p>
+								<p className=" font-semibold text-yellow-400 ">
+									We can&apos;t wait to hear from you!
+								</p>
 						</div>
 					</div>
 					{/* form */}
@@ -68,9 +68,7 @@ const Contact = () => {
 							</div>
 
 							<button className="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50">
-								<Link
-									to="mailto:george.boholteanu@gmail.com"									
-								>
+								<Link to="mailto:george.boholteanu@gmail.com">
 									get in touch
 								</Link>
 							</button>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -50,7 +50,7 @@ const Login = () => {
 
 	return (
 		<>
-			<div className="mx-auto max-w-screen-xl min-h-screen px-4 py-16 sm:px-6 lg:px-8 -mb-36 sm:mt-0">
+			<div className="mx-auto max-w-screen-xl min-h-screen px-4 py-16 sm:px-6 lg:px-8 -mb-20 sm:mt-0">
 				<div className="mx-auto max-w-lg">
 					<h1 className="flex-inline text-center text-2xl font-bold sm:text-3xl">
 						Welcome to{" "}
