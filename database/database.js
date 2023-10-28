a2892658f0ab5770b7d7cb840c9d6143f3bb8b36
George Boholteanu <39237040+georgeboholteanu@users.noreply.github.com>
2023-10-28 11:01:48 +0100

    Update database.js

--- database/database.js
+++ database/database.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 // Load environment variables from .env file
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_DBNAM, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const db = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
 });
