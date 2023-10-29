16e34a202c7033323aafec8f9097e57ecd96d7e7
georgeboholteanu <george.boholteanu@gmail.com>
2023-10-29 21:51:21 +0000

    update server address

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
 		proxy: {
 			"/api": {
 				// target: 'http://localhost:8000',
-				target: 'http://fitnest-app.vercel.app',
+				target: 'https://fitnest-server.onrender.com',
 				changeOrigin: true,
 			},
 		},
