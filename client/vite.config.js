635b1341d089614b471cb3eae87eafbf95075065
georgeboholteanu <george.boholteanu@gmail.com>
2023-10-29 16:41:26 +0000

    configured routes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
@@ -8,7 +9,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "fitnest-app.vercel.app",
+				target: 'fitnest-app.vercel.app',
 				changeOrigin: true,
 			},
 		},
